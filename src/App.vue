be43b22e3d5f27186942c151f6983d5a0898c7fb
heromanxxq <18888300623@163.com>
2020-04-26 10:37:23 +0800

    项目目录准备

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,11 @@
 export default {
   name: 'App',
   components: {
-    
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/css/base.css';
 </style>
